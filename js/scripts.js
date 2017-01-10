37f9fa9a6868e37b418f2b52e0c2e26c39309fc7
Alexandra Holcombe and Marcel Nogueira <marcel@>
2017-01-10 14:07:48 -0800

    add definition of operators and variables add jquery code to flip operators card

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  $(".js-card").click(function() {
+    $(".js-card").toggle();
+  });
+  $(".operators-card").click(function(){
+    $(".operators-card").toggle();
+  });
+});
